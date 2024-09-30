bd651b0103f38e0bf168591a7eb657f89a31ca05
cristian contreras <cr.ontreras@gmail.com>
2024-09-30 18:48:55 -0500

    class 6. useValue & useClass

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module'
 
+const API_KEY = '123456789'
+const API_KEY_PROD = 'PROD-123456789'
+
 @Module({
   imports: [UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      // useValue: API_KEY  -----> Forma estática
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY // Forma dinámica
+    }
+  ],
 })
 export class AppModule {}
