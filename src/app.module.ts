307b46172f9262e3d0ff156062d8ee8b561e03a9
cristian contreras <cr.ontreras@gmail.com>
2024-10-02 16:25:03 -0500

    class 7. useFactory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, HttpService, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -8,7 +8,7 @@ const API_KEY = '123456789'
 const API_KEY_PROD = 'PROD-123456789'
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [HttpModule, UsersModule, ProductsModule],
   controllers: [AppController],
   providers: [
     AppService,
@@ -16,6 +16,17 @@ const API_KEY_PROD = 'PROD-123456789'
       provide: 'API_KEY',
       // useValue: API_KEY  -----> Forma estática
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY // Forma dinámica
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const tasks = await http
+          .get('https://jsonplaceholder.typicode.com/todos')
+          .toPromise()
+
+        return tasks.data
+      }, 
+      inject: [HttpService]
     }
   ],
 })
